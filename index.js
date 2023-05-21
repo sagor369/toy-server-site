8a27d0fff397a8251e359cfc80e025893159a6bf
Sagor <sagor.heaking@gmail.com>
2023-05-21 10:45:13 +0600

    subcategory section complete

--- index.js
+++ index.js
@@ -27,7 +27,65 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    client.connect();
+
+
+    const database = client.db("toysCategory");
+    const clientCategory = database.collection("category");
+
+    app.get('/categorys', async(req, res) =>{
+        const client = clientCategory.find()
+        const result = await client.toArray()
+        res.send(result)
+
+    })
+
+    app.get('/categorys/:item', async(req, res) =>{
+        const id = req.params.item
+        const query = {
+            subCategory: id
+        }
+        const options = {
+            projection: { pictureUrl: 1, name: 1, price: 1, rating: 1, _id: 1 },
+        }
+        const client = clientCategory.find(query, options)
+        const result = await client.toArray()
+        res.send(result)
+
+    })
+    app.get('/user/:email', async(req, res) =>{
+        const userEmail = req.params.email
+        const myToy = {
+            sellerEmail: userEmail
+        }
+        const options = {
+            projection: { pictureUrl: 1, name: 1, price: 1, quantity: 1,subCategory: 1, _id: 1 },
+        }
+        const client = clientCategory.find(myToy, options)
+        const result = await client.toArray()
+        res.send(result)
+
+    })
+
+    // app.get('/user:/toy', async(req, res ) =>{
+    //     const userEmail = req.params.toy
+    //     console.log(userEmail) 
+    //     const myToy = {
+    //         sellerEmail: userEmail
+    //     }
+    //     const options = {
+    //         projection: { pictureUrl: 1, name: 1, price: 1, quantity: 1, subCategory: 1, _id: 1 },
+    //     }
+    //     const client = clientCategory.find(myToy, options)
+    //     const result = await client.toArray()
+    //     res.send(result)
+    // })
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
