33079480ad2ffdecb79144bcbf37e4e9142101ed
Sagor <sagor.heaking@gmail.com>
2023-05-26 21:26:33 +0600

    complete

--- index.js
+++ index.js
@@ -6,157 +6,168 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
-app.use(cors())
-app.use(express.json())
+    app.use(cors())
+    app.use(express.json())
 
 
-async function run() {
-    try {
-        client.connect()
+
+    const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsogw9w.mongodb.net/?retryWrites=true&w=majority`;
+
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+        });
+
+        async function run() {
+            try {
+                client.connect()
 
 
-        const database = client.db("toysCategory");
-        const clientCategory = database.collection("category");
+                const database = client.db("toysCategory");
+                const clientCategory = database.collection("category");
 
-        const reviewDB = client.db("reviewDB");
-        const reviews = reviewDB.collection("reviews");
+                const reviewDB = client.db("reviewDB");
+                const reviews = reviewDB.collection("reviews");
 
-        // home page toy url 
+                // home page toy url 
 
-        app.get('/review', async(req, res) =>{
-            const client = reviews.find()
-            const result = await client.toArray()
-            res.send(result)
-    
-        })
+                app.get('/review', async(req, res) =>{
+                        const client = reviews.find()
+                        const result = await client.toArray()
+                        res.send(result)
+                
+                    })
 
-    app.get('/data', async(req,res) =>{
-        const result = await clientCategory.estimatedDocumentCount();
-        res.send({totalData: result })
-    })
-
-    app.get('/toys', async(req, res) =>{
-        console.log(req.query)
-        const page = parseInt(req.query.page) || 0
-        const perpage = parseInt(req.query.perPage)
-        const skip = page * perpage
-        console.log(skip)
-            const result = await clientCategory.find().skip(skip).limit(perpage).toArray()
-            res.send(result)
-    
-        })
+                app.get('/data', async(req,res) =>{
+                    const result = await clientCategory.estimatedDocumentCount();
+                    res.send({totalData: result })
+                })
 
 
-    app.get('/toys/sort', async(req, res) =>{
-            const client = clientCategory.find({}, { sort: { price: 1 }})
-            const result = await client.toArray()
-            res.send(result)
-    
-        })
+                app.get('/toys', async(req, res) =>{
+                    console.log(req.query)
+                    const page = parseInt(req.query.page) || 0
+                    const perpage = parseInt(req.query.perPage)
+                    const skip = page * perpage
+                    console.log(skip)
+                        const result = await clientCategory.find().skip(skip).limit(perpage).toArray()
+                        res.send(result)
+                
+                    })
 
-        app.get('/toy/:id', async(req, res)=>{
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
+
+                app.get('/toys/sort', async(req, res) =>{
+                        const client = clientCategory.find({}, { sort: { price: 1 }})
+                        const result = await client.toArray()
+                        res.send(result)
+                
+                    })
+
+                app.get('/toy/:id', async(req, res)=>{
+                    const id = req.params.id;
+                    const query = { _id: new ObjectId(id) }
+                            
+                            const options = {
+                                projection: { pictureUrl: 1, quantity:1,  name: 1, price: 1, rating: 1, _id: 1, detailDescription: 1                                },
+                            }
+                            const client = clientCategory.find(query, options)
+                            const result = await client.toArray()
+                            res.send(result)
                     
-                    const options = {
-                        projection: { pictureUrl: 1, quantity:1,  name: 1, price: 1, rating: 1, _id: 1, detailDescription: 1                                },
+                })
+
+                    // subcategory toys list url 
+
+                    app.get('/toys/:item', async(req, res) =>{
+                            const itemCategory = req.params.item
+                            console.log(itemCategory)
+                            const query = {
+                                subCategory: itemCategory
+                            }
+                            const options = {
+                                projection: { pictureUrl: 1, name: 1, price: 1, rating: 1, _id: 1 },
+                            }
+                            const client = clientCategory.find(query, options)
+                            const result = await client.toArray()
+                            res.send(result)
+                    
+                        })
+
+                        app.get('/user/:email', async(req, res) =>{
+                            const userEmail = req.params.email
+                            const myToy = {
+                                sellerEmail: userEmail
+                            }
+                            const options = {
+                                projection: { pictureUrl: 1,sellerName: 1, sellerEmail: 1,  name: 1, price: 1, quantity: 1,subCategory: 1, _id: 1 },
+                            }
+                            const client = clientCategory.find(myToy, options)
+                            const result = await client.toArray()
+                            res.send(result)
+                    
+                        })
+
+                    app.post('/addtoy', async(req, res) =>{
+                        const data = req.body
+                        const result = await clientCategory.insertOne(data)
+                        res.send(result)
+                        
+                    })
+                    
+
+                    app.delete('/remove/:id', async(req, res) => {
+                        const id = req.params.id 
+                        const query = { _id: new ObjectId(id) }
+                        const result = await clientCategory.deleteOne(query)
+                        res.send(result)
+                    })
+
+                app.put('/update/:id', async(req, res) =>{
+                    console.log(req.body)
+                    const id = req.params.id;
+                    const filter = { _id: new ObjectId(id) };
+                    const name = req.body.name
+                    const price = req.body.price
+                    const quantity = req.body.quantity
+                    const detailDescription = req.body.detailDescription
+                    const updateData = {
+                        $set:{
+                            name,
+                            price,
+                            quantity,
+                            detailDescription
+
+                        }
                     }
-                    const client = clientCategory.find(query, options)
-                    const result = await client.toArray()
-                    res.send(result)
-            
-        })
+                    const result = await clientCategory.updateOne(filter ,updateData)
+                        res.send(result)
+
+                })
+                    
 
 
-        app.get('/toys/:item', async(req, res) =>{
-            const itemCategory = req.params.item
-            console.log(itemCategory)
-            const query = {
-                subCategory: itemCategory
+
+
+                await client.db("admin").command({ ping: 1 });
+                console.log("Pinged your deployment. You successfully connected to MongoDB!")
+
             }
-            const options = {
-                projection: { pictureUrl: 1, name: 1, price: 1, rating: 1, _id: 1 },
-            }
-            const client = clientCategory.find(query, options)
-            const result = await client.toArray()
-            res.send(result)
-    
-        })
-
-        app.get('/user/:email', async(req, res) =>{
-            const userEmail = req.params.email
-            const myToy = {
-                sellerEmail: userEmail
-            }
-            const options = {
-                projection: { pictureUrl: 1,sellerName: 1, sellerEmail: 1,  name: 1, price: 1, quantity: 1,subCategory: 1, _id: 1 },
-            }
-            const client = clientCategory.find(myToy, options)
-            const result = await client.toArray()
-            res.send(result)
-    
-        })
-
-        app.post('/addtoy', async(req, res) =>{
-            const data = req.body
-            const result = await clientCategory.insertOne(data)
-            res.send(result)
-            
-        })
-        
-
-        app.delete('/remove/:id', async(req, res) => {
-            const id = req.params.id 
-            const query = { _id: new ObjectId(id) }
-            const result = await clientCategory.deleteOne(query)
-            res.send(result)
-        })
-
-    app.put('/update/:id', async(req, res) =>{
-        console.log(req.body)
-        const id = req.params.id;
-        const filter = { _id: new ObjectId(id) };
-        const name = req.body.name
-        const price = req.body.price
-        const quantity = req.body.quantity
-        const detailDescription = req.body.detailDescription
-        const updateData = {
-            $set:{
-                name,
-                price,
-                quantity,
-                detailDescription
+            finally{
 
             }
         }
-        const result = await clientCategory.updateOne(filter ,updateData)
-            res.send(result)
+        run().catch(console.dir);
 
+
+
+    app.get('/', (req,res) =>{
+        res.send('hello touy senter')
     })
-        
 
 
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!")
-
-}
-finally{
-
-}
-}
-run().catch(console.dir);
-
-
-
-
-
-app.get('/', (req,res) =>{
-    res.send('hello touy senter')
-})
-
-
-app.listen(port, ()=>{
-    console.log(`toy projects in running port ${port}`)
-})
+    app.listen(port, ()=>{
+        console.log(`toy projects in running port ${port}`)
+    })
